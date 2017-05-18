bec4a07a1106ce8b45e3c8e453967f1f1cf36db6
shunryui nik <sourabh29wasnik@gmail.com>
2017-05-19 01:41:42 +0530

    My first commit

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var track = 0
+
+    @IBOutlet weak var theLabel: UILabel!
+    
+    
+    @IBAction func pushMe(_ sender: Any) {
+         theLabel.text = "Hey millionaire!"
+        self.view.backgroundColor = UIColor.green
+        track += 1
+        if track >= 10 {
+           theLabel.text = "You pushed the button more than 10 times and i am on acid"
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.gray
+       
     }
 
     override func didReceiveMemoryWarning() {
